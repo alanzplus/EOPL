3e97f7f2c5200d7952b3a5197ca42a7231303d59
Alan Xuan <jzxuanuni@gmail.com>
2019-05-01 15:00:54 +0800

    Assignment 2: union
    # Testing
    
    ```console
    racket -t a2-tests.rkt
    racket -t a2-student-tests.rkt
    ```

--- [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2-tests.rkt
+++ [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2-tests.rkt
@@ -8,6 +8,11 @@
     (test-suite "list-ref" 
                 (test-equal? "case1" (list-ref '(a b c) 2) 'c)
                 (test-equal? "case2" (list-ref '(a b c) 0) 'a))
+    (test-suite "union"
+                (test-equal? "case1" (union '() '()) '())
+                (test-equal? "case2" (union '(x) '()) '(x))
+                (test-equal? "case3" (union '(x) '(x)) '(x))
+                (test-equal? "case4" (union '(x y) '(x z)) '(z x y)))
 ))
 
 (run-tests tests)
--- [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2.rkt
+++ [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (provide list-ref)
+(provide union)
 
 (define list-ref
   (lambda (ls n)
@@ -11,3 +12,13 @@
              ls
              (cdr (nth-cdr (- n 1)))))))
       (car (nth-cdr n)))))
+
+(define union
+  (lambda (l1 l2)
+    (foldr
+      (lambda (e res)
+        (if (memv e res)
+          res
+          (cons e res)))
+      l1
+      l2)))
