73b342978a9312ebd4dc8f86e2352a59ee714d91
Alan Xuan <jzxuanuni@gmail.com>
2019-05-01 14:51:28 +0800

    Assignment 2: list-ref
    # Testing
    
    ```console
    racket -t a2-tests.rkt
    racket -t a2-student-tests.rkt
    ```

--- [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2-tests.rkt
+++ [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2-tests.rkt
@@ -1,11 +1,13 @@
 #lang racket
 
-(require rackunit "a1.rkt")
+(require rackunit "a2.rkt")
 (require rackunit/text-ui)
 
 (define tests
   (test-suite "A2:"
-              (test-suite "test")
+    (test-suite "list-ref" 
+                (test-equal? "case1" (list-ref '(a b c) 2) 'c)
+                (test-equal? "case2" (list-ref '(a b c) 0) 'a))
 ))
 
 (run-tests tests)
--- [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2.rkt
+++ [Course][Indiana] Programing Language/a2-Free, Bound, and Lexical Address/a2.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+(provide list-ref)
+
+(define list-ref
+  (lambda (ls n)
+    (letrec
+      ((nth-cdr
+         (lambda (n)
+           (if (eqv? n 0)
+             ls
+             (cdr (nth-cdr (- n 1)))))))
+      (car (nth-cdr n)))))
