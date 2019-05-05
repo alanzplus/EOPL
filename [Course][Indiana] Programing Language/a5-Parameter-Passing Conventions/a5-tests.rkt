21dec3a0f48deb4e6148009f7123bf7071ef6b38
Alan Xuan <jzxuanuni@gmail.com>
2019-05-06 00:35:57 +0800

    Assignment 5: Add more test cases for val-of-cbr

--- [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5-tests.rkt
+++ [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5-tests.rkt
@@ -37,7 +37,32 @@
                                                       x))
                                                  (empty-env))
                                        10)
-                          )
+                          (test-equal? "case5"
+                                (val-of-cbr
+                                  '((lambda (f)
+                                      ((lambda (g)
+                                         ((lambda (z) (begin2
+                                                        (g z)
+                                                        z))
+                                          55))
+                                       (lambda (y) (f y)))) (lambda (x) (set! x 44)))
+                                  (empty-env))
+                                44)
+                          (test-equal? "case6" (val-of-cbr
+                                                 '((lambda (swap)
+                                                     ((lambda (a)
+                                                        ((lambda (b)
+                                                           (begin2
+                                                             ((swap a) b)
+                                                             a)) 44)) 33))
+                                                   (lambda (x)
+                                                     (lambda (y)
+                                                       ((lambda (temp)
+                                                          (begin2
+                                                            (set! x y)
+                                                            (set! y temp))) x))))
+                                                 (empty-env))
+                                       44))
 ))
 
 (run-tests tests)
