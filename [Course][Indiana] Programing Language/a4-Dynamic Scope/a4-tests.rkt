26c4a181bfe6be7c1ecf38f11cf093d02f969a2c
Alan Xuan <jzxuanuni@gmail.com>
2019-05-05 11:18:24 +0800

    Assignment 4: lex
    # Testing
    
    ```console
    racket -t a4-tests.rkt
    racket -t a4-student-tests.rkt
    ```

--- [Course][Indiana] Programing Language/a4-Dynamic Scope/a4-tests.rkt
+++ [Course][Indiana] Programing Language/a4-Dynamic Scope/a4-tests.rkt
@@ -5,7 +5,31 @@
 
 (define tests
   (test-suite "A4:"
+              (test-suite "lex"
+                          (test-equal? "case1" (lex '((lambda (x) x) 5)  '()) '((lambda (var 0)) (const 5)))
+                          (test-equal? "case2"
+                                      (lex '(lambda (!)
+                                              (lambda (n)
+                                                (if (zero? n) 1 (* n (! (sub1 n))))))
+                                           '())
+                                      '(lambda
+                                         (lambda
+                                           (if (zero? (var 0))
+                                             (const 1)
+                                             (* (var 0) ((var 1) (sub1 (var 0))))))))
+                          (test-equal? "case3"
+                                      (lex '(let ((! (lambda (!)
+                                                       (lambda (n)
+                                                         (if (zero? n) 1 (* n ((! !) (sub1 n))))))))
+                                              ((! !) 5))
+                                           '())
+                                      '(let (lambda
+                                              (lambda
+                                                (if (zero? (var 0))
+                                                  (const 1)
+                                                  (* (var 0) (((var 1) (var 1)) (sub1 (var 0)))))))
+                                         (((var 0) (var 0)) (const 5)))))
 
-)))
+))
 
 (run-tests tests)
--- [Course][Indiana] Programing Language/a4-Dynamic Scope/a4.rkt
+++ [Course][Indiana] Programing Language/a4-Dynamic Scope/a4.rkt
@@ -1,1 +1,34 @@
 #lang racket
+
+(provide lex)
+
+(define lex
+  (lambda (expr ctx)
+    (match expr
+      [n #:when (number? n) `(const ,n)]
+      [`(zero? ,expr1) `(zero? ,(lex expr1 ctx))]
+      [`(sub1 ,expr1) `(sub1 ,(lex expr1 ctx))]
+      [`(* ,expr1 ,expr2) `(* ,(lex expr1 ctx) ,(lex expr2 ctx))]
+      [`(if ,pred-expr ,then-expr ,else-expr) `(if ,(lex pred-expr ctx) ,(lex then-expr ctx) ,(lex else-expr ctx))]
+      [`(let ,bindings ,body)
+        `(let
+           ; Seeing from the test case, it assumes that let only allows single binding
+           ,(car
+              (map
+                (lambda (binding)
+                  (lex (cadr binding) ctx))
+                bindings))
+           ,(lex
+              body
+              (foldr
+                (lambda (binding new-ctx)
+                  (cons (car binding) new-ctx))
+                ctx
+                bindings)))]
+      [`(lambda (,id) ,body)
+        `(lambda ,(lex body (cons id ctx)))]
+      [`(,exp1 ,exp2)
+        `(,(lex exp1 ctx) ,(lex exp2 ctx))]
+      [`,id
+        (let ([idx (index-of ctx id)])
+          (if idx (list 'var idx) id))])))
