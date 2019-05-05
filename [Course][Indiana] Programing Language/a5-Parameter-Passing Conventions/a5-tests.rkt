5dcac7ec9f5dc5a9f6aa7b199038afb33e16b1b0
Alan Xuan <jzxuanuni@gmail.com>
2019-05-05 22:18:47 +0800

    Assignment 5: Call-By-Reference
    # Testing
    
    ```console
    racket -t a5-tests.rkt
    racket -t a5-student-tests.rkt
    ```

--- [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5-tests.rkt
+++ [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5-tests.rkt
@@ -5,7 +5,39 @@
 
 (define tests
   (test-suite "A5:"
-
+              (test-suite "call-by-reference"
+                          ;; Making sure set! works
+                          (test-equal? "case1" (val-of-cbr
+                                                 '((lambda (x) (begin2 (set! x #t)
+                                                                       (if x 3 5))) #f)
+                                                 (empty-env))
+                                       3)
+                          ;; Returns 4 under CBR...
+                          (test-equal? "case2" (val-of-cbr
+                                                 '((lambda (a)
+                                                     ((lambda (p)
+                                                        (begin2
+                                                          (p a)
+                                                          a)) (lambda (x) (set! x 4)))) 3)
+                                                 (empty-env))
+                                       4)
+                          (test-equal? "case3" (val-of-cbr
+                                                 '(let ([x 3])
+                                                    (begin2
+                                                      (set! x 4)
+                                                      x))
+                                                 (empty-env))
+                                       4)
+                          (test-equal? "case4" (val-of-cbr
+                                                 '(let ([x 3])
+                                                    (begin2
+                                                      ((lambda (y)
+                                                         (set! y 10))
+                                                      x)
+                                                      x))
+                                                 (empty-env))
+                                       10)
+                          )
 ))
 
 (run-tests tests)
--- [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5.rkt
+++ [Course][Indiana] Programing Language/a5-Parameter-Passing Conventions/a5.rkt
@@ -1,1 +1,73 @@
 #lang racket
+
+(provide val-of-cbr)
+(provide empty-env)
+
+(define val-of-cbr
+  (lambda (expr env)
+    (match expr
+      [num #:when (number? num) num]
+      [var #:when (symbol? var) (unbox (apply-env env var))]
+      [b #:when (boolean? b) b]
+      [`(* ,expr1 ,expr2)
+        (* (val-of-cbr expr1 env) (val-of-cbr expr2 env))]
+      [`(sub1 ,expr1)
+        (sub1 (val-of-cbr expr1 env))]
+      [`(zero? ,expr1)
+        (zero? (val-of-cbr expr1 env))]
+      [`(if ,pred-expr ,then-expr ,else-expr)
+        (if (val-of-cbr pred-expr env)
+          (val-of-cbr then-expr env)
+          (val-of-cbr else-expr env))]
+      [`(random ,n)
+        (random (val-of-cbr n env))]
+      [`(begin2 ,expr1 ,expr2)
+        (begin
+          (val-of-cbr expr1 env)
+          (val-of-cbr expr2 env))]
+      [`(set! ,var ,expr1)
+        (set-box! (apply-env env var) (val-of-cbr expr1 env))]
+      [`(lambda (,var) ,body)
+        (make-closure-cbr var body env)]
+      [`(let ,bindings ,body)
+        (val-of-cbr
+          body
+          (foldr
+            (lambda (binding new-env)
+              (let
+                ([var (car binding)]
+                 [val (val-of-cbr (cadr binding) env)])
+                (extend-env var (box val) new-env)))
+            env
+            bindings))]
+      [`(,expr1 ,var) #:when (symbol? var)
+        (apply-closure (val-of-cbr expr1 env) (apply-env env var))]
+      [`(,expr1 ,expr2)
+        (apply-closure (val-of-cbr expr1 env) (box (val-of-cbr expr2 env)))])))
+
+(define make-closure-cbr
+  (lambda (var body env)
+    (lambda (arg)
+      (val-of-cbr
+        body
+        (extend-env var arg env)))))
+
+(define apply-closure
+  (lambda (closure arg)
+    (closure arg)))
+
+(define empty-env
+  (lambda ()
+    (lambda (var)
+      (error "cannot find binding of ~s" var))))
+
+(define extend-env
+  (lambda (saved-var val env)
+    (lambda (search-var)
+      (if (eqv? search-var saved-var)
+        val
+        (apply-env env search-var)))))
+
+(define apply-env
+  (lambda (env var)
+    (env var)))
