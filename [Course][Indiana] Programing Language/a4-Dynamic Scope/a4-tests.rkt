5070ca4344dc3d558a930fd2216559e5310398b8
Alan Xuan <jzxuanuni@gmail.com>
2019-05-05 12:43:46 +0800

    Assignemnt 4: value-of-fn
    # Notes
    Implement closure using procedural representation
    
    # Testing
    
    ```console
    racket -t a4-tests.rkt
    racket -t a4-student-tests.rkt
    ```

--- [Course][Indiana] Programing Language/a4-Dynamic Scope/a4-tests.rkt
+++ [Course][Indiana] Programing Language/a4-Dynamic Scope/a4-tests.rkt
@@ -30,6 +30,60 @@
                                                   (* (var 0) (((var 1) (var 1)) (sub1 (var 0)))))))
                                          (((var 0) (var 0)) (const 5)))))
 
+              (test-suite "value-of"
+                          (test-equal? "case1" (value-of-fn 
+                                                 '((lambda (x) (if (zero? x) 
+                                                                 12 
+                                                                 47)) 
+                                                   0) 
+                                                 (empty-env))
+                                       12)
+                          (test-equal? "case2"
+                                       (value-of-fn
+                                         '(let ([y (* 3 4)])
+                                            ((lambda (x) (* x y)) (sub1 6)))
+                                         (empty-env))
+                                       60)
+                          (test-equal? "case3"
+                                       (value-of-fn
+                                         '(let ([x (* 2 3)])
+                                            (let ([y (sub1 x)])
+                                              (* x y)))
+                                         (empty-env))
+                                       30)
+                          (test-equal? "case4"
+                                       (value-of-fn
+                                         '(let ([x (* 2 3)])
+                                            (let ([x (sub1 x)])
+                                              (* x x)))
+                                         (empty-env))
+                                       25)
+                          (test-equal? "case5"
+                                       (value-of-ds
+                                         '((lambda (x) (if (zero? x) 
+                                                         12 
+                                                         47)) 
+                                           0) 
+                                         (empty-env))
+                                       12)
+                          (test-equal? "case6" (value-of-ds
+                                                 '(let ([y (* 3 4)])
+                                                    ((lambda (x) (* x y)) (sub1 6)))
+                                                 (empty-env))
+                                       60)
+                          (test-equal? "case7" (value-of-ds
+                                                 '(let ([x (* 2 3)])
+                                                    (let ([y (sub1 x)])
+                                                      (* x y)))
+                                                 (empty-env))
+                                       30)
+                          (test-equal? "case8" (value-of-ds
+                                                 '(let ([x (* 2 3)])
+                                                    (let ([x (sub1 x)])
+                                                      (* x x)))
+                                                 (empty-env))
+                                       25))
+
 ))
 
 (run-tests tests)
--- [Course][Indiana] Programing Language/a4-Dynamic Scope/a4.rkt
+++ [Course][Indiana] Programing Language/a4-Dynamic Scope/a4.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
 (provide lex)
+(provide value-of-fn)
+(provide closure-fn)
+(provide apply-closure-fn)
+(provide empty-env)
 
 (define lex
   (lambda (expr ctx)
@@ -32,3 +36,58 @@
       [`,id
         (let ([idx (index-of ctx id)])
           (if idx (list 'var idx) id))])))
+
+(define value-of-fn
+  (lambda (expr env)
+    (match expr
+      [num #:when (number? num) num]
+      [var #:when (symbol? var) (apply-env env var)]
+      [b #:when (boolean? b) b]
+      [`(lambda (,id) ,body) (closure-fn id body env)]
+      [`(let ,bindings ,body)
+        (value-of-fn
+          body
+          (foldr
+            (lambda (binding aggregate-env)
+              (let ([id (car binding)]
+                    [val (value-of-fn (cadr binding) env)])
+                (extend-env id val aggregate-env)))
+            env
+            bindings))]
+      [`(sub1 ,expr1)
+        (- (value-of-fn expr1 env) 1)]
+      [`(* ,expr1 ,expr2)
+        (* (value-of-fn expr1 env) (value-of-fn expr2 env))]
+      [`(if ,pred-expr ,then-expr ,else-expr)
+        (if (value-of-fn pred-expr env)
+          (value-of-fn then-expr env)
+          (value-of-fn else-expr env))]
+      [`(zero? ,expr1)
+        (zero? (value-of-fn expr1 env))]
+      [`(,expr1 ,expr2)
+        (apply-closure-fn (value-of-fn expr1 env) (value-of-fn expr2 env))])))
+
+(define closure-fn
+  (lambda (var body env)
+    (lambda (arg)
+      (value-of-fn body (extend-env var arg env)))))
+
+(define apply-closure-fn
+  (lambda (closure arg)
+    (closure arg)))
+
+(define empty-env
+  (lambda ()
+    (lambda (var)
+      (error "No binding for ~s" var))))
+
+(define extend-env
+  (lambda (id val env)
+    (lambda (var)
+      (if (eqv? var id)
+        val
+        (apply-env env var)))))
+
+(define apply-env
+  (lambda (env var)
+    (env var)))
