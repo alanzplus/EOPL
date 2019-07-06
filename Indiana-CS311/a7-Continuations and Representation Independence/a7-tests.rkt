f11a2d2ea8e575c39d37b32686adc8db0acc7a4e
Alan Xuan <jzxuanuni@gmail.com>
2019-07-06 22:26:17 +0900

    Assignment 7: last-non-zero
    ### Testing
    * racket -t a7-tests.rkt
    * racket -t a7-student-tests.rkt

--- Indiana-CS311/a7-Continuations and Representation Independence/a7-tests.rkt
+++ Indiana-CS311/a7-Continuations and Representation Independence/a7-tests.rkt
@@ -4,6 +4,22 @@
 (require rackunit/text-ui)
 
 (define tests
-  (test-suite "A7:"))
+  (test-suite "A7:"
+              (test-suite "last-non-zero"
+                          (test-equal? "case1"
+                                       (last-non-zero '()) '())
+                          (test-equal? "case2"
+                                       (last-non-zero '(1 2 3 4 5)) '(1 2 3 4 5))
+                          (test-equal? "case3"
+                                       (last-non-zero '(0)) '())
+                          (test-equal? "case4"
+                                       (last-non-zero '(0 1)) '(1))
+                          (test-equal? "case5"
+                                       (last-non-zero '(1 0 1 2 3 4)) '(1 2 3 4))
+                          (test-equal? "case6"
+                                       (last-non-zero '(1 0 2 0 3 0 4)) '(4))
+                          )
+              )
+  )
 
 (run-tests tests)
--- Indiana-CS311/a7-Continuations and Representation Independence/a7.rkt
+++ Indiana-CS311/a7-Continuations and Representation Independence/a7.rkt
@@ -1,1 +1,16 @@
 #lang racket
+(provide last-non-zero)
+
+(define last-non-zero
+  (lambda (ls)
+    (let/cc k
+            (letrec
+              ([helper
+                 (lambda (ls)
+                   (cond
+                     [(null? ls) '()]
+                     [(eq? (car ls) 0)
+                      (k (helper (cdr ls)))]
+                     [else
+                       (cons (car ls) (helper (cdr ls)))]))])
+              (helper ls)))))
