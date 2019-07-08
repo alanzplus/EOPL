5a59dde9d2dfd9024392f8843ab42b66afe0f257
Alan Xuan <jzxuanuni@gmail.com>
2019-07-08 21:55:40 +0900

    [CS311][Assignment 7] Replace high-order continuation with continuation builder
    ### Notes
    * Inner/Nested conntinuations are NOT replaced by builder (just don't borther to do it)
    
    ### Testing
    * racket -t a7-tests.rkt
    * racket -t a7-student-tests.rkt

--- Indiana-CS311/a7-Continuations and Representation Independence/a7.rkt
+++ Indiana-CS311/a7-Continuations and Representation Independence/a7.rkt
@@ -94,38 +94,63 @@
     (match expr
            [`(const ,val) (apply-k cont val)]
            [`(mult ,x1 ,x2)
-             (value-of-cps x1 env (lambda (v1)
-                                    (value-of-cps x2 env (lambda (v2)
-                                                           (apply-k cont (* v1 v2))))))]
-           [`(sub1 ,x) (value-of-cps x env (lambda (v1)
-                                             (apply-k cont (- v1 1))))]
-           [`(zero ,x) (value-of-cps x env (lambda (v1)
-                                             (apply-k cont (zero? v1))))]
-           [`(if ,test ,conseq ,alt)
-             (value-of-cps test env (lambda (v1)
-                                      (if v1
-                                          (value-of-cps conseq env (lambda (v2) (apply-k cont v2)))
-                                          (value-of-cps alt env (lambda (v3) (apply-k cont v3))))))]
-           [`(letcc ,body)
-             (value-of-cps body
-                           (extend-env cont env)
-                           (lambda (v) (apply-k cont v)))]
-           [`(throw ,k-exp ,v-exp)
-             (value-of-cps k-exp env (lambda (v1)
-                                       (value-of-cps v-exp env (lambda (v2)
-                                                                 (v1 v2)))))]
-           [`(let ,e ,body) 
-             (value-of-cps e env (lambda (v1)
-                                   (value-of-cps body
-                                                 (extend-env v1 env)
-                                                 (lambda (v2) (apply-k cont v2)))))]
+             (value-of-cps x1 env (mult-cont x1 x2 env cont))]
+           [`(sub1 ,x) (value-of-cps x env (sub1-cont cont))]
+           [`(zero ,x) (value-of-cps x env (zero-cont cont))]
+           [`(if ,test ,conseq ,alt) (value-of-cps test env (if-cont conseq alt env cont))]
+           [`(letcc ,body) (value-of-cps body (extend-env cont env) (letcc-cont cont))]
+           [`(throw ,k-exp ,v-exp) (value-of-cps k-exp env (throw-cont env cont v-exp))]
+           [`(let ,e ,body) (value-of-cps e env (let-cont env cont body))]
            [`(lambda ,body)
              (apply-k cont (make-closure body env))]
-           [`(app ,rator ,rand)
-             (value-of-cps rator env (lambda (v1)
-                                       (value-of-cps rand env (lambda (v2)
-                                                                (apply-closure v1 v2 cont)))))]
+           [`(app ,rator ,rand) (value-of-cps rator env (app-cont env cont rand))]
            [`(var ,address) (apply-k cont (apply-env env address))])))
+
+; Procedure Presentation for Continuation
+(define mult-cont
+  (lambda (x1 x2 env saved-cont)
+    (lambda (v1)
+      (value-of-cps x2 env (lambda (v2)
+                             (apply-k saved-cont (* v1 v2)))))))
+
+(define sub1-cont
+  (lambda (saved-cont)
+    (lambda (v1)
+      (apply-k saved-cont (- v1 1)))))
+
+(define zero-cont
+  (lambda (saved-cont)
+    (lambda (v1)
+      (apply-k saved-cont (zero? v1)))))
+
+(define if-cont
+  (lambda (conseq alt env saved-cont)
+    (lambda (v1)
+      (if v1
+          (value-of-cps conseq env (lambda (v2) (apply-k saved-cont v2)))
+          (value-of-cps alt env (lambda (v2) (apply-k saved-cont v2)))))))
+
+(define letcc-cont
+  (lambda (saved-cont)
+    (lambda (v1)
+      (apply-k saved-cont v1))))
+
+(define throw-cont
+  (lambda (env saved-cont v-exp)
+    (lambda (v1)
+      (value-of-cps v-exp env (lambda (v2) (v1 v2))))))
+
+(define let-cont
+  (lambda (env cont body)
+    (lambda (v1)
+      (value-of-cps body
+                    (extend-env v1 env)
+                    (lambda (v2) (apply-k cont v2))))))
+
+(define app-cont
+  (lambda (env saved-cont rand)
+    (lambda (v1)
+      (value-of-cps rand env (lambda (v2) (apply-closure v1 v2 saved-cont))))))
 
 (define make-closure
   (lambda (body env)
@@ -135,21 +160,20 @@
                     (lambda (v1)
                       (apply-k k v1))))))
 
-  (define apply-env
-    (lambda (env address)
-      (env address)))
+(define apply-env
+  (lambda (env address)
+    (env address)))
 
-  (define extend-env
-    (lambda (val env)
-      (lambda (y)
-        (if (zero? y) val
-            (env (sub1 y))))))
+(define extend-env
+  (lambda (val env)
+    (lambda (y)
+      (if (zero? y) val
+          (env (sub1 y))))))
 
-  (define apply-closure
-    (lambda (rator rand cont)
-      (rator rand cont)))
+(define apply-closure
+  (lambda (rator rand cont)
+    (rator rand cont)))
 
-  (define apply-k
-    (lambda (cont v)
-      (cont v)))
-
+(define apply-k
+  (lambda (cont v)
+    (cont v)))
