b6fa436053ebbf4bc9f8c54dee6d716fc9758039
Alan Xuan <jzxuanuni@gmail.com>
2019-07-20 18:29:44 +0900

    [CS311][Assignment 8] Apply Representation Independent Continuation to depth
    ### Testing
    * racket -t a8-tests.rkt

--- Indiana-CS311/a8-Registerization/a8-tests.rkt
+++ Indiana-CS311/a8-Registerization/a8-tests.rkt
@@ -10,17 +10,35 @@
       [(zero? n) (ack (sub1 m) 1 k)]
       [else (ack m (sub1 n) (lambda (v) (ack (sub1 m) v k)))])))
 
+(define depth
+  (lambda (ls k)
+    (cond
+      [(null? ls) (k 1)]
+      [(pair? (car ls))
+       (depth (car ls)
+              (lambda (l)
+                (depth (cdr ls)
+                       (lambda (r)
+                         (let ((l (add1 l)))
+                           (if (< l r) (k r) (k l)))))))]
+      [else (depth (cdr ls) k)])))
+
 (define tests
   (test-suite "A8:"
-              (test-suite ""
+              (test-suite "ack"
                           (test-equal? "case1"
-                                      (ack-reg-driver 2 2) 7)
+                                       (ack-reg-driver 2 2) 7)
                           (test-equal? "case2"
                                        (ack-reg-driver 1 1) (ack 1 1 (lambda (v) v)))
                           (test-equal? "case3"
                                        (ack-reg-driver 3 3) (ack 3 3 (lambda (v) v)))
                           )
+              (test-suite "depth"
+                          (test-equal? "case1"
+                                       (depth-reg-driver '(1 (2 (3 (4))))) (depth '(1 (2 (3 (4)))) (lambda (v) v)))
+                          (test-equal? "case2"
+                                       (depth-reg-driver '(1 2 2 (2 (3 (4))))) (depth '(1 2 2 (2 (3 (4)))) (lambda (v) v))))
               )
-)
+  )
 
 (run-tests tests)
--- Indiana-CS311/a8-Registerization/a8.rkt
+++ Indiana-CS311/a8-Registerization/a8.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (provide ack-reg-driver)
+(provide depth-reg-driver)
 
 (define ack-reg-driver
   (letrec ([m* 'uninit]
@@ -39,3 +40,23 @@
         (set! n* n)
         (set! k* (empty-k))
         (ack)))))
+
+(define depth-reg-driver
+  (letrec ([empty-k (lambda () '(empty-k))]
+           [big-k (lambda (ls k) `(big-k ,ls ,k))]
+           [small-k (lambda (l k) `(small-k ,l ,k))]
+           [apply-k (lambda (k v)
+                      (match k
+                             ['(empty-k) v]
+                             [`(big-k ,ls ,k)
+                               (depth (cdr ls) (small-k v k))]
+                             [`(small-k ,l ,k)
+                               (let ([l (add1 l)])
+                                 (if (< l v) (apply-k k v) (apply-k k l)))]))]
+           [depth (lambda (ls k)
+                    (cond
+                      [(null? ls) (apply-k k 1)]
+                      [(pair? (car ls)) (depth (car ls) (big-k ls k))]
+                      [else (depth (cdr ls) k)]))])
+    (lambda (ls)
+      (depth ls (empty-k)))))
