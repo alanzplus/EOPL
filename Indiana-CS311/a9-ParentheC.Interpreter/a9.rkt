eddb2045aed9ad27c46891a3785bc91967331908
Alan Xuan <jzxuanuni@gmail.com>
2019-07-25 17:51:50 +0900

    [CS311][Assignment 9][Transform value-of-cps] Step 6: Registerization
    ### Notes
    * registerized value-of-cps
    * registerized apply-k
    * registerized apply-env
    * registerized apply-closure
    
    ### Tests
    * racket -t a9.rkt

--- Indiana-CS311/a9-ParentheC.Interpreter/a9.rkt
+++ Indiana-CS311/a9-ParentheC.Interpreter/a9.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require "parenthec.rkt")
 
+(define-registers expr^ address^ env^ cont^ v^ closure^)
+
 (define-union expr
               (const cexp)
               (var n)
@@ -19,26 +21,31 @@
               (extend val saved-env))
 
 (define apply-env
-  (lambda (e address)
-    (union-case e env
+  (lambda ()
+    (union-case env^ env
                 [(empty) (error 'value-of-cps "unbound identifier")]
                 [(extend val saved-env)
-                 (if (zero? address) val
-                     (let* ([e saved-env]
-                            [address (sub1 address)])
-                       (apply-env e address)))])))
+                 (if (zero? address^)
+                     (begin
+                       (set! cont^ cont^)
+                       (set! v^ val)
+                       (apply-k))
+                     (begin (set! env^ saved-env)
+                            (set! address^ (sub1 address^))
+                            (set! cont^ cont^)
+                            (apply-env)))])))
 
 (define-union closure
               (procedure body env))
 
 (define apply-closure
-  (lambda (p v cont)
-    (union-case p closure
+  (lambda ()
+    (union-case closure^ closure
                 [(procedure body env)
-                 (let* ([expr^ body]
-                        [env^ (env_extend v env)]
-                        [cont^ (kt_closure cont)])
-                   (value-of-cps expr^ env^ cont^))])))
+                 (begin (set! expr^ body)
+                        (set! env^ (env_extend v^ env))
+                        (set! cont^ (kt_closure cont^))
+                        (value-of-cps))])))
 
 (define-union kt
               (empty)
@@ -59,160 +66,159 @@
               (closure saved-cont))
 
 (define apply-k
-  (lambda (cont v)
-    (union-case cont kt
-                [(empty) v]
+  (lambda ()
+    (union-case cont^ kt
+                [(empty) v^]
                 [(mult x2 saved-env saved-cont)
-                 (let ([expr^ x2]
-                       [env^ saved-env]
-                       [cont^ (kt_mult-inner v saved-cont)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ x2)
+                        (set! env^ saved-env)
+                        (set! cont^ (kt_mult-inner v^ saved-cont))
+                        (value-of-cps))]
                 [(mult-inner v1 saved-cont)
-                 (let* ([cont saved-cont]
-                        [v (* v1 v)])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ (* v1 v^))
+                        (apply-k))]
                 [(sub1 saved-cont)
-                 (let* ([cont saved-cont]
-                        [v (- v 1)])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ (- v^ 1))
+                        (apply-k))]
                 [(zero saved-cont)
-                 (let* ([cont saved-cont]
-                        [v (zero? v)])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ (zero? v^))
+                        (apply-k))]
                 [(if conseq alt saved-env saved-cont)
-                 (if v
-                     (let* ([expr^ conseq]
-                            [env^ saved-env]
-                            [cont^ (kt_if-conseq saved-cont)])
-                       (value-of-cps expr^ env^ cont^))
-                     (let* ([expr^ alt]
-                            [env^ saved-env]
-                            [cont^ (kt_if-alt saved-cont)])
-                       (value-of-cps expr^ env^ cont^)))]
+                 (if v^
+                     (begin (set! expr^ conseq)
+                            (set! env^ saved-env)
+                            (set! cont^ (kt_if-conseq saved-cont))
+                            (value-of-cps))
+                     (begin (set! expr^ alt)
+                            (set! env^ saved-env)
+                            (set! cont^ (kt_if-alt saved-cont))
+                            (value-of-cps)))]
                 [(if-conseq saved-cont)
-                 (let* ([cont saved-cont]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ v^)
+                        (apply-k))]
                 [(if-alt saved-cont)
-                 (let* ([cont saved-cont]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ v^)
+                        (apply-k))]
                 [(letcc saved-cont)
-                 (let* ([cont saved-cont]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ v^)
+                        (apply-k))]
                 [(throw saved-env saved-cont v-exp)
-                 (let* ([expr^ v-exp]
-                        [env^ saved-env]
-                        [cont^ (kt_throw-inner saved-cont v)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ v-exp)
+                        (set! env^ saved-env)
+                        (set! cont^ (kt_throw-inner saved-cont v^))
+                        (value-of-cps))]
                 [(throw-inner saved-cont v1)
-                 (let* ([cont v1]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin (set! cont^ v1)
+                        (set! v^ v^)
+                        (apply-k))]
                 [(let saved-env saved-cont body)
-                 (let* ([expr^ body]
-                        [env^ (env_extend v saved-env)]
-                        [cont^ (kt_let-inner saved-cont)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ body)
+                        (set! env^ (env_extend v^ saved-env))
+                        (set! cont^ (kt_let-inner saved-cont))
+                        (value-of-cps))]
                 [(let-inner saved-cont)
-                 (let* ([cont saved-cont]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin 
+                   (set! cont^ saved-cont)
+                   (set! v^ v^)
+                   (apply-k))]
                 [(app saved-env saved-cont rand)
-                 (let* ([expr^ rand]
-                        [env^ saved-env]
-                        [cont^ (kt_app-inner v saved-cont)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ rand)
+                        (set! env^ saved-env)
+                        (set! cont^ (kt_app-inner v^ saved-cont))
+                        (value-of-cps))]
                 [(app-inner v1 saved-cont)
-                 (let* ([p v1]
-                        [v v]
-                        [cont saved-cont])
-                   (apply-closure p v cont))]
+                 (begin (set! closure^ v1)
+                        (set! v^ v^)
+                        (set! cont^ saved-cont)
+                        (apply-closure))]
                 [(closure saved-cont)
-                 (let* ([cont saved-cont]
-                        [v v])
-                   (apply-k cont v))]
+                 (begin (set! cont^ saved-cont)
+                        (set! v^ v^)
+                        (apply-k))]
                 )))
 
 (define value-of-cps
-  (lambda (expr^ env^ cont^)
+  (lambda ()
     (union-case expr^ expr
                 [(const val)
-                 (let* ([cont cont^]
-                        [v val])
-                   (apply-k cont v))]
+                 (begin (set! cont^ cont^)
+                        (set! v^ val)
+                        (apply-k))]
                 [(mult x1 x2)
-                 (let* ([expr^ x1]
-                        [env^ env^]
-                        [cont^ (kt_mult x2 env^ cont^)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ x1)
+                        (set! env^ env^)
+                        (set! cont^ (kt_mult x2 env^ cont^))
+                        (value-of-cps))]
                 [(sub1 x)
-                 (let* ([expr^ x]
-                        [env^ env^]
-                        [cont^ (kt_sub1 cont^)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ x)
+                        (set! env^ env^)
+                        (set! cont^ (kt_sub1 cont^))
+                        (value-of-cps))]
                 [(zero x)
-                 (let* ([expr^ x]
-                        [env^ env^]
-                        [cont^ (kt_zero cont^)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ x)
+                        (set! env^ env^)
+                        (set! cont^ (kt_zero cont^))
+                        (value-of-cps))]
                 [(if test conseq alt)
-                 (let* ([expr^ test]
-                        [conseq conseq]
-                        [alt alt]
-                        [env^ env^]
-                        [cont^ (kt_if conseq alt env^ cont^)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ test)
+                        (set! conseq conseq)
+                        (set! alt alt)
+                        (set! env^ env^)
+                        (set! cont^ (kt_if conseq alt env^ cont^))
+                        (value-of-cps))]
                 [(letcc body)
-                 (let* ([expr^ body]
-                        [env^ (env_extend cont^ env^)]
-                        [cont^ (kt_letcc cont^)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ body)
+                        (set! env^ (env_extend cont^ env^))
+                        (set! cont^ (kt_letcc cont^))
+                        (value-of-cps))]
                 [(throw k-exp v-exp)
-                 (let* ([expr^ k-exp]
-                        [env^ env^]
-                        [cont^ (kt_throw env^ cont^ v-exp)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ k-exp)
+                        (set! env^ env^)
+                        (set! cont^ (kt_throw env^ cont^ v-exp))
+                        (value-of-cps))]
                 [(let e body)
-                 (let* ([expr^ e]
-                        [body body]
-                        [env^ env^]
-                        [cont^ (kt_let env^ cont^ body)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ e)
+                        (set! env^ env^)
+                        (set! cont^ (kt_let env^ cont^ body))
+                        (value-of-cps))]
                 [(lambda body)
-                 (let* ([cont cont^]
-                        [v (closure_procedure body env^)])
-                   (apply-k cont v))]
+                 (begin (set! cont^ cont^)
+                        (set! v^ (closure_procedure body env^))
+                        (apply-k))]
                 [(app rator rand)
-                 (let* ([expr^ rator]
-                        [env^ env^]
-                        [cont^ (kt_app env^ cont^ rand)])
-                   (value-of-cps expr^ env^ cont^))]
+                 (begin (set! expr^ rator)
+                        (set! env^ env^)
+                        (set! cont^ (kt_app env^ cont^ rand))
+                        (value-of-cps))]
                 [(var address)
-                 (let* ([cont cont^]
-                        [v (let* ([e env^]
-                                  [address address])
-                             (apply-env e address))])
-                   (apply-k cont v))])))
+                 (begin (set! env^ env^)
+                        (set! address^ address)
+                        (set! cont^ cont^)
+                        (apply-env))])))
 
 (define main 
   (lambda ()
-    (let* ([expr^ (expr_let 
-                    (expr_lambda
-                      (expr_lambda 
-                        (expr_if
-                          (expr_zero (expr_var 0))
-                          (expr_const 1)
-                          (expr_mult (expr_var 0) (expr_app (expr_app (expr_var 1) (expr_var 1)) (expr_sub1 (expr_var 0)))))))
-                    (expr_mult
-                      (expr_letcc
-                        (expr_app
-                          (expr_app (expr_var 1) (expr_var 1))
-                          (expr_throw (expr_var 0) (expr_app (expr_app (expr_var 1) (expr_var 1)) (expr_const 4)))))
-                      (expr_const 5)))]
-           [env^ (env_empty)]
-           [cont^ (kt_empty)])
-      (value-of-cps expr^ env^ cont^))))
+    (begin (set! expr^ (expr_let 
+                         (expr_lambda
+                           (expr_lambda 
+                             (expr_if
+                               (expr_zero (expr_var 0))
+                               (expr_const 1)
+                               (expr_mult (expr_var 0) (expr_app (expr_app (expr_var 1) (expr_var 1)) (expr_sub1 (expr_var 0)))))))
+                         (expr_mult
+                           (expr_letcc
+                             (expr_app
+                               (expr_app (expr_var 1) (expr_var 1))
+                               (expr_throw (expr_var 0) (expr_app (expr_app (expr_var 1) (expr_var 1)) (expr_const 4)))))
+                           (expr_const 5))))
+           (set! env^ (env_empty))
+           (set! cont^ (kt_empty))
+           (value-of-cps))))
 
 (main)
