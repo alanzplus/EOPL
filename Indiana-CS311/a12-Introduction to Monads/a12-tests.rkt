405773bdb907671ba8c52c00513186f2d0e2cc78
Alan Xuan <jzxuanuni@gmail.com>
2019-08-13 21:14:40 +0800

    [CS311][Assignment 12] Maybe Monad
    ### Testing
    * racket -t a12-tests.rkt

--- Indiana-CS311/a12-Introduction to Monads/a12-tests.rkt
+++ Indiana-CS311/a12-Introduction to Monads/a12-tests.rkt
@@ -1,10 +1,22 @@
 #lang racket
 
 (require rackunit "a12.rkt")
+(require rackunit "monads.rkt")
 (require rackunit/text-ui)
 
 
 (define tests
-  (test-suite "A12:"))
+  (test-suite "A12:"
+              (test-suite "findf-maybe"
+                          (test-equal? "case1"
+                                       (findf-maybe symbol? '(1 2 c))
+                                       (Just 'c))
+                          (test-equal? "case2"
+                                       (findf-maybe boolean? '(#f 1 2 c))
+                                       (Just #f))
+                          (test-equal? "case3"
+                                       (findf-maybe number? '(a b c))
+                                       (Nothing)))
+              ))
 
 (run-tests tests)
--- Indiana-CS311/a12-Introduction to Monads/a12.rkt
+++ Indiana-CS311/a12-Introduction to Monads/a12.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+(require "monads.rkt")
+(provide findf-maybe)
+
+(define findf-maybe
+  (lambda (predicate? ls)
+    (cond
+      [(null? ls) (Nothing)]
+      [(predicate? (car ls))
+       (Just (car ls))]
+      [else
+        (findf-maybe predicate? (cdr ls))])))
